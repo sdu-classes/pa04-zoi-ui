179bb2203f51bb7e3c84e24fd0f39f28135b2083
zoi-ui <124429965+zoi-ui@users.noreply.github.com>
2023-02-25 15:33:18 +0600

    Create Main.java

---
+++ problem-C/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class prac4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Name:  ");
+        String name = s.nextLine();
+
+        System.out.print("Age:  ");
+        int age = s.nextInt();
+
+        System.out.print("Hobby:  ");
+        String hobby = s.next();
+
+        System.out.print("Enter you favourite sport  ");
+        String sport = s.next();
+
+
+
+        SportAndHobbyImpl sportAndHobby = new SportAndHobbyImpl();
+        sportAndHobby.setNaame(name);
+        sportAndHobby.setAge(age);
+        sportAndHobby.setMyHobby(hobby);
+        sportAndHobby.setMyFavoriteSport(sport);
+        int cost = sportAndHobby.howMuchItCostToPlayThisSport();
+        System.out.println(sportAndHobby);
+
+
+
+
+
+    }
+}
