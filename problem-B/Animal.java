89b974e0911401f0d02b68f28d8a3dfc138df485
zoi-ui <124429965+zoi-ui@users.noreply.github.com>
2023-02-25 15:39:28 +0600

    Update Animal.java

---
+++ problem-B/Animal.java
@@ -0,0 +1,12 @@
+abstract class Animal{
+    protected int legs;
+
+    protected Animal(int legs) {
+        
+
+    }
+    public void walk(){
+        System.out.println("this animal walk");
+    }
+    public void eat(){}
+}
