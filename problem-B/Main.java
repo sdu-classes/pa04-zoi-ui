2c6e81969652d534cb28f38009fbb21fae8c162f
zoi-ui <124429965+zoi-ui@users.noreply.github.com>
2023-02-25 15:42:58 +0600

    Create Main.java

---
+++ problem-B/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class prac4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+
+        Fish d = new Fish();
+        Cat c = new Cat("Fluffy");
+        Animal a = new Fish();
+        Animal e = new Spider();
+        Pet p = new Cat();
+
+        d.eat();
+        d.walk();
+
+
+        c.eat();
+        c.walk();
+
+
+        a.eat();
+        a.walk();
+
+        e.eat();
+        e.walk();
+
+
+        p.play();
+        System.out.println(p.getName()); // Fluffy
+        p.setName("Larry");
+        System.out.println(p.getName()); // Mittens
+
+        Animal f = new Spider();
+        f.walk();
+
+
+
+
+    }
+}
