b84972520db44fab499577df0bd3fbda6a80fd45
zoi-ui <124429965+zoi-ui@users.noreply.github.com>
2023-02-25 14:11:09 +0600

    MovableCircle

---
+++ problem-A/MovableCircle.java
@@ -0,0 +1,36 @@
+class MovableCircle   implements Movable{
+    private int radius;
+    private MovablePoint center;
+    MovableCircle(int x , int y , int xSpeed , int ySpeed, int radius){
+        this.center = new MovablePoint(x , y , xSpeed , ySpeed);
+        this.radius =radius;
+    }
+
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+    @Override
+    public String toString() {
+         return String.format("(%s),radius=%d", center.toString(), radius);
+    }
+}
